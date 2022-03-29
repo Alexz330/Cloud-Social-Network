9d0860048a7cd4ea4c9eb22b84a1c220eaaea124
alexz330 <alexislc330@gmail.com>
2022-03-29 12:23:33 -0600

    agreando seguridad

--- auth/index.js
+++ auth/index.js
@@ -1,10 +1,51 @@
+
 const jwt = require('jsonwebtoken')
 
+
+const secret = config.jwt
 const sing = (data)=>{
     return jwt.sign(data,'secreto')
 
 }
 
-module.exports = {
+
+const check = {
+  own: function(req,owner){
+
+
+  }
+}
+
+const verify = (token)=>{
+  return jwt.verify(token)
+}
+
+const getToken = (auth)=>{
+
+  if(!auth){
+    throw new Error('No viene token'); 
+  }
+
+    
+  if(auth.indexOf('Bearer') === -1){
+     throw new Error('Formato invalido'); 
+  }
+  let token =  auth.replace('Bearer','');
+
+  return token;
+}
+
+
+const decoderHeader = (req)=>{
+
+  const authorization = req.headers.authorization || '';
+  const token = getToken(authorization);
+  const decoded = verify(token);
+  req.user = decoded;
+
+  return decoded
+
+}
+  module.exports = {
     sing, 
 };
