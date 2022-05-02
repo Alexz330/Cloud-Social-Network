41d0e0e1fca3e980853c744906262048721ed9ea
Alexz330 <alexislc330@gmail.com>
2022-05-01 21:17:33 -0600

    se agrego autentificacion

--- auth/index.js
+++ auth/index.js
@@ -1,10 +1,43 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const config = require('../config')
+const sing = (data) => {
+    return jwt.sign(data, secret)
+}
 
-const sing = (data)=>{
-    return jwt.sign(data,'secreto')
+const secret = config.jwt.secret;
 
+const verify = (token) => {
+    return jwt.verify(token,secret)
+}
+
+const check = {
+    own: function (req, owner) {
+        const decoded = decoderHeader(req);
+        console.log(decoded);
+    }
+}
+const getToken = (auth) => {
+    if(!auth){
+        throw  new Error('no vienve tokenc')
+    }
+    if(auth.indexOf('Bearer')=== -1){
+        throw new Error('Formato invalido')
+    }
+    let token = auth.replace('Bearer',"");
+
+    return token;
+}
+
+const decoderHeader = (req) => {
+    const authorization = req.headers.authorization || "";
+    const token = getToken(authorization);
+    const decoded = verify(token);
+    req.user = decoded;
+    return decoded
+
+    
 }
 
 module.exports = {
-    sing, 
+    sing,
 };
